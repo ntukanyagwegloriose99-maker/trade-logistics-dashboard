64ad21e0457a4069a102323f113ef7aa77187bf4
ntukanyagwegloriose99-maker <ntukanyagwegloriose99@gmail.com>
2026-01-13 16:43:01 +0200

    Remove unused plotly imports from dashboard.py
    Removed unused imports for plotly.

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+
 
 # ==================== PAGE CONFIG ====================
 st.set_page_config(
